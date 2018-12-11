e26e06cfa9fa3cc8a2c33dd18db31716c1420ca8
Eduardo Tirta <edu.tirta@gmail.com>
2018-12-11 00:06:12 -0200

    Add files via upload

--- entrega2/proj2/train.py
+++ entrega2/proj2/train.py
@@ -2,13 +2,13 @@ from gluoncv import model_zoo, data, utils
 import pickle
 import os
 
-dataset = './imgs'
+dataset = './images'
 model = model_zoo.get_model('yolo3_darknet53_coco', pretrained=True)
 dic = {}
 for pasta in os.listdir(dataset):
     if(pasta[0] != '.'):
         for imgs in os.listdir(dataset + '/' + pasta):
-            image_path = dataset + '/' + pasta +'/' + images
+            image_path = dataset + '/' + pasta +'/' + imgs
             x, img = data.transforms.presets.yolo.load_test(image_path, short=512)
             class_IDs, scores, bounding_boxs = model(x)
             scores = scores.asnumpy()
